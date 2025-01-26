6a1bbb211cc4628412d2146c1f027832ae1213a8
Kaneasamoorthy Vithursan <vithursit@gmail.com>
2025-01-26 21:39:17 +0530

    first java program

---
+++ intro.java
@@ -0,0 +1,7 @@
+public class intro{
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("this is the first java program");
+
+    }
+}
