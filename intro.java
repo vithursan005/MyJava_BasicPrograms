db5ea32e8841534d8ff4ac9d8d83f8c3d4267d48
Vithursan_kanesamoorthy <vithursit@gmail.com>
2025-01-26 21:45:07 +0530

    fix: changed grammatical error

--- intro.java
+++ intro.java
@@ -1,7 +1,6 @@
 public class intro{
     public static void main(String[] args) {
-        System.out.println("hello world");
-        System.out.println("this is the first java program");
-
+        System.out.println("Hello world");
+        System.out.println("This is the first java program");
     }
 }
